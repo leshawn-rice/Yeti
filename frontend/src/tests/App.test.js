de18ae9d2206e2bf47dd588c97143f28417102cf
leshawn-rice <leshawnrice3@gmail.com>
2021-04-22 21:45:51 -0700

    configured travis yml file

--- frontend/src/tests/App.test.js
+++ frontend/src/tests/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from '../components/App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-// });
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Yeti/i);
+  expect(linkElement).toBeInTheDocument();
+});
