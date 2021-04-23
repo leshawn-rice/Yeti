272f3cf147d80d025901d179fb4621ddc7ccd0b0
leshawn-rice <leshawnrice3@gmail.com>
2021-04-22 21:16:31 -0700

    Added download icons to mobile site

--- frontend/src/components/Routes/Home.js
+++ frontend/src/components/Routes/Home.js
@@ -1,6 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import ios from '../../img/download-ios.svg';
+import android from '../../img/download-android.svg';
+import '../../styles/Home.css'
+
 const Home = () => {
+  const [mobileView, setMobileView] = useState(false);
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setMobileView(true)
+        : setMobileView(false);
+    };
+    setResponsiveness();
+    window.addEventListener("resize", () => setResponsiveness());
+  }, []);
+
+  const displayMobile = () => {
+    return (
+      <div className="Downloads">
+        <img className="Home-Download-Image" src={ios} />
+        <img className="Home-Download-Image" src={android} />
+      </div>
+    )
+  }
+
+  const displayDesktop = () => {
+    return (
+      <h1>Hello World</h1>
+    )
+  }
+
   return (
-    <h1>Hello World</h1>
+    <>
+      { mobileView ? displayMobile() : displayDesktop()}
+    </>
   )
 }
 
