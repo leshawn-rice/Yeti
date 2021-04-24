a9dc1ead1bf8ca63c1d0adfc19e2342dbab864d5
leshawn-rice <leshawnrice3@gmail.com>
2021-04-23 22:47:01 -0700

    wrapped app test in provider to pass test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,14 @@
+// External Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from "redux-thunk";
+// Internal Dependencies
 import rootReducer from './redux/rootReducer';
+// Components
 import App from './components/App';
+// Styles
 import './styles/index.css';
 
 const store = createStore(
--- frontend/src/tests/App.test.js
+++ frontend/src/tests/App.test.js
@@ -1,8 +1,20 @@
+// External Dependencies
 import { render, screen } from '@testing-library/react';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from "redux-thunk";
+// Internal Dependencies
+import rootReducer from '../redux/rootReducer';
+// Component
 import App from '../components/App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  const store = createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(thunk)
+    ));
+  render(<Provider store={store}><App /></Provider>);
   const linkElement = screen.getByText(/Yeti/i);
   expect(linkElement).toBeInTheDocument();
 });
