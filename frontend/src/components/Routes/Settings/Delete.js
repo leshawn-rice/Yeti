ab45e5b2daf8d94b703e320937b36f30c91fd5a6
leshawn-rice <leshawnrice3@gmail.com>
2021-05-04 12:44:18 -0700

    Made settings headers same size for each setting page

--- frontend/src/components/Routes/Settings/Delete.js
+++ frontend/src/components/Routes/Settings/Delete.js
@@ -25,7 +25,7 @@ const Delete = () => {
   return (
     <div className="Delete-Account">
       <Modal toggled={isModalOpen} toggleModal={setIsModalOpen} content={modalContent} />
-      <h1>Permanently delete your account</h1>
+      <h1 className="Delete-Header">Permanently delete your account</h1>
       <p className="Delete-Info">You won't be able to reverse this action. <strong>All</strong> posts, comments, and votes from this account will be permanently deleted from our servers, and will <strong>not</strong> be able to be retrieved.</p>
       <button onClick={toggleModal} className="Delete-Account-Button">Delete</button>
     </div>
