40420ca1e968c606d161b88a28e227d00a6a8740
leshawn-rice <leshawnrice3@gmail.com>
2021-05-13 17:58:34 -0700

    made tests pass B)

--- frontend/src/tests/App.test.js
+++ frontend/src/tests/App.test.js
@@ -14,6 +14,6 @@ test('renders learn react link', () => {
         <App />
       </PersistGate>
     </Provider>);
-  const linkElement = screen.getAllByText(/Yeti/i);
+  const linkElement = screen.getAllByText(/Yeti/i)[0];
   expect(linkElement).toBeInTheDocument();
 });
