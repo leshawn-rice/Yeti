2128257a9f2e7a1bbcaa9d8a128270a2e947b462
leshawn-rice <leshawnrice3@gmail.com>
2021-05-16 21:53:10 -0700

    added test email to config

--- backend/config.js
+++ backend/config.js
@@ -9,8 +9,8 @@ const EARTH_RADIUS = 3958.8;
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 const PORT = +process.env.PORT || 3001;
 const SERVER_EMAIL = {
-  email: process.env.EMAIL || null,
-  password: process.env.EMAIL_PASSWORD || null
+  email: process.env.EMAIL || 'test@test.com',
+  password: process.env.EMAIL_PASSWORD || 'testpassword'
 }
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
