ca7111968bd961ee7e3646408d6205f7d561386d
leshawn-rice <leshawnrice3@gmail.com>
2021-06-11 22:56:37 -0700

    edited readme, added docs to frontend action creators

--- README.md
+++ README.md
@@ -1,8 +1,64 @@
-# Yeti
+# Yeti - Engage with your Community
+
 [![Build Status](https://travis-ci.com/leshawn-rice/yeti.svg?branch=main)](https://travis-ci.com/leshawn-rice/yeti) [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) [![GitHub issues open](https://img.shields.io/github/issues/leshawn-rice/yeti)](https://github.com/leshawn-rice/yeti/issues)
 
-## Yeti is an app that empowers people to share ideas, opinions, and thoughts with others in their community
+[<img src="frontend/public/favicon.ico"> (Click to Visit) ](https://goyeti.app/)
 
-## Issues:
+### Yeti brings communities together through location-based interaction!
 
-1. None ATM
+# The What
+
+### Yeti is a full-stack application developed with both desktop, and mobile experiences in mind. Yeti uses users' locations to connect them with others in their surrounding areas, and allows them to post, comment, rate, and save content that others in their area create!
+
+# The How
+
+## Backend
+### - Nodejs
+### - Express
+### - PostgreSQL
+### - Bcrypt & JWT
+### - Heroku
+
+## Frontend
+### - React
+### - Redux
+### - FontAwesome
+
+## API
+### - IPINFO API
+### - Window Navigator API
+
+# The Why
+
+## Backend
+For the backend, I chose to use a Node/Express setup on the backend because:
+ - Keeping the backend/frontend both in Javascript makes switching between the two faster and more efficient
+ - Nodejs can handle asynchronous operations while Python cannot, and this project needed operations to be handled asynchronously on the backend (handling email,login,registration, and posting operations all at once rather than sequentially)
+
+I used PostgreSQL for the database because, since I'm dealing with users, posts, and comments, all of which can be managed easily with classes, an ORDBMS like Postgres allowed me to take advantage of OOP concepts in handling my database.
+
+I hosted the backend on Heroku because Heroku is a cheap, scalable way to quickly push a live project
+
+## Frontend
+For the frontend, I chose to use React because it's use of components allowed me to reuse the logic for posts & comments, as well as develop a SPA using React-Router. 
+
+I chose to use Redux (and Redux-Persist) instead of the useContext hook and localstorage because I had many components on different levels in the component hierarchy that needed access to things like the user, loaded posts, comments etc. and redux stores its state in the global scope. Additionally, since redux-persist manages to keep state persistent across visits without much logic, it was simpler and more compatible with redux than using something like localstorage or cookies.
+
+# Using Yeti
+
+### To download Yeti, simply do the following: 
+1. Download the code and run ```npm install``` in the terminal in both the backend and frontend folders.
+2. Seed the database by navigating to the backend folder, and running ```psql < yeti.sql```
+3. Open up the backend folder, and run ```nodemon```
+4. Open up the frontend folder, and run ```npm start```
+5. Run tests with the command ```npm run test``` in either folder.
+
+# User Flow
+
+The general user flow for Yeti goes as follows: 
+
+1. Login/Sign Up if not already logged in
+2. Confirm email if that hasn't already been done
+3. Check your local area for interesting posts/comments
+4. Uprate, Downrate, and Save posts/comments you find interesting
+5. Check out your profile and see if your rating has increased and the ratings of your posts/comments
--- frontend/src/redux/actionCreators.js
+++ frontend/src/redux/actionCreators.js
@@ -29,6 +29,14 @@ import {
   REFRESH,
 } from './actionTypes';
 
+/**
+ * 
+ * Called at the start of any action that interacts with the backend API
+ * 
+ * Starts the loading screen, and clears any errors currently in state
+ * 
+ */
+
 const startApiAction = () => {
   return async function (dispatch) {
     try {
@@ -41,6 +49,14 @@ const startApiAction = () => {
   }
 }
 
+/**
+ * 
+ * Called at the end of any action that interacts with the backend API
+ * 
+ * Ends the loading screen, and handles any errors as necessary
+ * 
+ */
+
 const endApiAction = () => {
   return async function (dispatch) {
     try {
@@ -52,12 +68,30 @@ const endApiAction = () => {
   }
 }
 
+/**
+ * 
+ * @param {array} errs 
+ * 
+ * handles any errors thrown in the process of interacting with the backend API
+ * 
+ * Stops the loading screen and shows the errors
+ * 
+ */
+
 const handleApiErrors = (errs) => {
   return async function (dispatch) {
     dispatch(stopLoading());
     dispatch(showErrors(errs));
   }
 }
+
+/**
+ * 
+ * Uses the Geolocater to get the user's current location. If an error occurs using the IPInfo API,
+ * attempts to use the window navigator API to get the location, if that fails, shows the user an error message.
+ * 
+ * Otherwise stores the location in state
+ */
 
 const getLocationApi = () => {
   return async function (dispatch) {
@@ -79,6 +113,15 @@ const getLocationApi = () => {
   }
 }
 
+/**
+ * 
+ * @param {string} oldToken 
+ * @param {string} username 
+ * 
+ * gets a new token and new user data from the backend api using the current token and a username. 
+ * Stores the new user in state
+ */
+
 const refreshUserApi = (oldToken, username) => {
   return async function (dispatch) {
     try {
@@ -92,6 +135,14 @@ const refreshUserApi = (oldToken, username) => {
     }
   }
 }
+
+/**
+ * 
+ * @param {object} userData 
+ * 
+ * given the userData, registers the user on the backend and stores the new user and token
+ * in state
+ */
 
 const registerUserApi = (userData) => {
   return async function (dispatch) {
@@ -107,6 +158,14 @@ const registerUserApi = (userData) => {
   }
 }
 
+/**
+ * 
+ * @param {object} userData 
+ * 
+ * given the userData, logs in the user on the backend and stores the new user and token
+ * in state
+ */
+
 const loginUserApi = (userData) => {
   return async function (dispatch) {
     try {
@@ -120,6 +179,15 @@ const loginUserApi = (userData) => {
     }
   }
 }
+
+/**
+ * 
+ * @param {string} oldToken 
+ * @param {string} username 
+ * @param {string} email 
+ * 
+ * Interacts with the backend API to update the user's email, and stores the new token & user in state
+ */
 
 const updateEmailApi = (oldToken, username, email) => {
   return async function (dispatch) {
@@ -135,11 +203,21 @@ const updateEmailApi = (oldToken, username, email) => {
   }
 }
 
-const updatePasswordApi = (oldToken, username, oldPassword, newPassworrd) => {
+/**
+ * 
+ * @param {string} oldToken 
+ * @param {string} username 
+ * @param {string} oldPassword 
+ * @param {string} newPassword 
+ * 
+ * Interacts with the backend API to update the user's password, and stores the new token & user in state
+ */
+
+const updatePasswordApi = (oldToken, username, oldPassword, newPassword) => {
   return async function (dispatch) {
     try {
       dispatch(startApiAction());
-      const { token, user } = await YetiApi.updatePassword(oldToken, username, oldPassword, newPassworrd);
+      const { token, user } = await YetiApi.updatePassword(oldToken, username, oldPassword, newPassword);
       dispatch(updateUser({ token, user }));
       dispatch(endApiAction());
     }
@@ -148,6 +226,12 @@ const updatePasswordApi = (oldToken, username, oldPassword, newPassworrd) => {
     }
   }
 }
+
+/**
+ * 
+ * @param {int} id 
+ * Interacts with the backend API to get the user with the given ID and returns them
+ */
 
 const getUserByIdApi = (id) => {
   return async function (dispatch) {
@@ -162,6 +246,11 @@ const getUserByIdApi = (id) => {
     }
   }
 }
+
+/**
+ * 
+ * Interacts with the backend API to delete the user from the DB and removes them from state
+ */
 
 const deleteUserApi = (token, username) => {
   return async function (dispatch) {
@@ -178,6 +267,15 @@ const deleteUserApi = (token, username) => {
   }
 }
 
+/**
+ * 
+ * @param {object} location 
+ * 
+ * Interacts with the backend API to get all posts within the distance specified in "location" to the lat/lon
+ * specified in "location" and adds them to state
+ * 
+ */
+
 const getLocalPostsApi = (location) => {
   return async function (dispatch) {
     try {
@@ -191,6 +289,11 @@ const getLocalPostsApi = (location) => {
     }
   }
 }
+
+/**
+ * 
+ * Interacts with the backend API to create a comment in the DB and adds it to state
+ */
 
 const createPostApi = (token, username, postData) => {
   return async function (dispatch) {
@@ -207,6 +310,12 @@ const createPostApi = (token, username, postData) => {
   }
 }
 
+/**
+ * 
+ * Interacts with the backend API to uprate the post in the DB and updates it in state
+ */
+
+
 const upratePostApi = (token, user_id, post_id) => {
   return async function (dispatch) {
     try {
@@ -220,6 +329,12 @@ const upratePostApi = (token, user_id, post_id) => {
     }
   }
 }
+
+/**
+ * 
+ * Interacts with the backend API to downrate the post in the DB and updates it in state
+ */
+
 
 const downratePostApi = (token, user_id, post_id) => {
   return async function (dispatch) {
@@ -235,6 +350,11 @@ const downratePostApi = (token, user_id, post_id) => {
   }
 }
 
+/**
+ * 
+ * Interacts with the backend API to add a saved_post entry to the DB and adds it to state
+ */
+
 const savePostApi = (token, username, id, user_id) => {
   return async function (dispatch) {
     try {
@@ -249,6 +369,11 @@ const savePostApi = (token, username, id, user_id) => {
   }
 }
 
+/**
+ * 
+ * Interacts with the backend API to delete the saved_post entry from the DB and removes it from state
+ */
+
 const unsavePostApi = (token, username, id, user_id) => {
   return async function (dispatch) {
     try {
@@ -262,6 +387,11 @@ const unsavePostApi = (token, username, id, user_id) => {
     }
   }
 }
+
+/**
+ * 
+ * Interacts with the backend API to delete the post from the DB and removes it from state
+ */
 
 const deletePostApi = (token, username, id) => {
   return async function (dispatch) {
@@ -278,6 +408,12 @@ const deletePostApi = (token, username, id) => {
   }
 }
 
+/**
+ * 
+ * Interacts with the backend API to uprate the comment in the DB and updates it in state
+ */
+
+
 const uprateCommentApi = (token, user_id, comment_id) => {
   return async function (dispatch) {
     try {
@@ -291,6 +427,11 @@ const uprateCommentApi = (token, user_id, comment_id) => {
     }
   }
 }
+
+/**
+ * 
+ * Interacts with the backend API to downrate the comment in the DB and updates it in state
+ */
 
 const downrateCommentApi = (token, user_id, comment_id) => {
   return async function (dispatch) {
@@ -306,6 +447,11 @@ const downrateCommentApi = (token, user_id, comment_id) => {
   }
 }
 
+/**
+ * 
+ * Interacts with the backend API to create a comment in the DB and adds it to state
+ */
+
 const createCommentApi = (token, username, postData) => {
   return async function (dispatch) {
     try {
@@ -319,6 +465,11 @@ const createCommentApi = (token, username, postData) => {
     }
   }
 }
+
+/**
+ * 
+ * Interacts with the backend API to add a saved_comment entry to the DB and adds it to state
+ */
 
 const saveCommentApi = (token, username, id, user_id) => {
   return async function (dispatch) {
@@ -334,6 +485,11 @@ const saveCommentApi = (token, username, id, user_id) => {
   }
 }
 
+/**
+ * 
+ * Interacts with the backend API to delete the saved_post entry from the DB and removes it from state
+ */
+
 const unsaveCommentApi = (token, username, id, user_id) => {
   return async function (dispatch) {
     try {
@@ -347,6 +503,11 @@ const unsaveCommentApi = (token, username, id, user_id) => {
     }
   }
 }
+
+/**
+ * 
+ * Interacts with the backend API to delete the comment from the DB and removes it from state
+ */
 
 const deleteCommentApi = (token, username, id) => {
   return async function (dispatch) {
@@ -362,6 +523,15 @@ const deleteCommentApi = (token, username, id) => {
     }
   }
 }
+
+/**
+ * 
+ * @param {string} token 
+ * @param {int} id 
+ * 
+ * Gets all the post data (user, comments, rating etc) from the backend API and stores it
+ * in state
+ */
 
 const getFullPostApi = (token, id) => {
   return async function (dispatch) {
